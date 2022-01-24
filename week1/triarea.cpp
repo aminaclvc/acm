9d40d1e1bdb922b099e14f0c812ca9adff023200
Amina Colovic <74953948+aminaclvc@users.noreply.github.com>
2022-01-24 21:40:55 +0100

    add triarea

---
+++ week1/triarea.cpp
@@ -0,0 +1,21 @@
+/* Amina Colovic
+* Kattis Problem: Triangle Area
+* Problem ID: triarea
+* CPU Time limit: 1 sec
+* Memory limit: 1024 MB
+* Difficulty: 1.4
+* Language: C++
+*/
+
+#include <iostream>
+
+int main(){
+  int h = 0;
+  int b = 0;
+
+  std::cin >> h >> b;
+
+  std::cout << 0.5 * h * b << std::endl;
+
+  return 0;
+}
