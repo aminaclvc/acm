3bb2b4310de686dbbfb9455bb5eeefc366e90254
Amina Colovic <74953948+aminaclvc@users.noreply.github.com>
2022-01-24 21:23:57 +0100

    add hello

---
+++ week1/hello.c
@@ -0,0 +1,16 @@
+/* Amina Colovic
+* Kattis Problem: Hello World!
+* Problem ID: hello
+* CPU Time limit: 5 sec
+* Memory limit: 1024 MB
+* Difficulty: 6.5
+* Language: C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World!\n");
+}
