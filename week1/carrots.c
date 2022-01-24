6f75a906dd43b5e98a4314634b639f5e1e7ed2ae
Amina Colovic <74953948+aminaclvc@users.noreply.github.com>
2022-01-24 22:07:13 +0100

    add carrots

---
+++ week1/carrots.c
@@ -0,0 +1,24 @@
+/* Amina Colovic
+* Kattis Problem: Solving for Carrots
+* Problem ID: carrots
+* CPU Time limit: 1 sec
+* Memory limit: 1024 MB
+* Difficulty: 1.3
+* Language: C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int n, p = 0;
+
+  scanf("%d %d", &n, &p);
+  for(int i = 0; i < n; i++)
+  {
+          char description[1000];
+          scanf("%s", description);
+  }
+  printf("%d\n",p);
+}
